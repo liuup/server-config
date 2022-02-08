fb2a071cb21ab24ddb5fe338e0ea6fe24fdd0779
UP <akarealup@gmail.com>
2022-02-08 22:11:45 +0800

    Update server-config.sh

--- server-config.sh
+++ server-config.sh
@@ -5,3 +5,25 @@ sudo apt-get upgrade
 sudo apt-get install npm
 
 sudo apt-get install nodejs
+
+# 配置node清华源
+# 设定环境变量
+export NODE_MIRROR=https://mirrors.tuna.tsinghua.edu.cn/nodejs-release/
+
+# 然后正常使用 n 即可
+sudo n stable
+
+
+## 配置python fastapi
+pip install fastapi
+
+sudo apt-get install uvicorn
+
+pip install uvloop
+
+pip install httptools==0.1.*
+
+
+
+
+
